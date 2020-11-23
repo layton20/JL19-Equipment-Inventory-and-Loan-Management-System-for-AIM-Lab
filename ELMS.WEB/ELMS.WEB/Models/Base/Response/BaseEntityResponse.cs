08d855f1ba27ffbc443e35e0e6c3e3f713158e0e
Johnny Le <johnnyxdblack@gmail.com>
2020-11-23 12:01:44 +0000

    Added base response classes

---
+++ ELMS.WEB/ELMS.WEB/Models/Base/Response/BaseEntityResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ELMS.WEB.Models.Base.Response
+{
+    public class BaseEntityResponse
+    {
+        public Guid UID { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public DateTime AmendedTimestamp { get; set; }
+    }
+}
---
+++ ELMS.WEB/ELMS.WEB/Models/Base/Response/BaseResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ELMS.WEB.Models.Base.Response
+{
+    public class BaseResponse
+    {
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
