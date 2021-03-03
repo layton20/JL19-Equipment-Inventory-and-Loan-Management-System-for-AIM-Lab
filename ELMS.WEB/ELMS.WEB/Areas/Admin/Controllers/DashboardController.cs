bbb72b50e4690f40bb3b8b6396ba62ebe3311289
Johnny Le <contact.lejohnny@gmail.com>
2021-03-03 21:44:54 +0000

    Simple dashboard page

---
+++ ELMS.WEB/ELMS.WEB/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using ELMS.WEB.Areas.Admin.Models.Dashboard;
+using ELMS.WEB.Managers.Equipment.Interfaces;
+using ELMS.WEB.Managers.Loan.Interface;
+using ELMS.WEB.Models.Loan.Response;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NsEquipment = ELMS.WEB.Areas.Equipment;
+using NsLoan = ELMS.WEB.Areas.Loan;
+
+namespace ELMS.WEB.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly IMapper __Mapper;
+        private readonly IEquipmentManager __EquipmentManager;
+        private readonly ILoanManager __LoanManager;
+
+        public DashboardController(IMapper mapper, IEquipmentManager equipmentManager, ILoanManager loanManager)
+        {
+            __Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            __EquipmentManager = equipmentManager ?? throw new ArgumentNullException(nameof(equipmentManager));
+            __LoanManager = loanManager ?? throw new ArgumentNullException(nameof(loanManager));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel _Model = new IndexViewModel
+            {
+                EquipmentList = __Mapper.Map<IList<NsEquipment.Models.EquipmentViewModel>>((await __EquipmentManager.GetAsync()).Equipments),
+                Loans = __Mapper.Map<IList<NsLoan.Models.LoanViewModel>>(await __LoanManager.GetAsync()),
+            };
+
+            return View("Index", _Model);
+        }
+    }
+}
---
+++ ELMS.WEB/ELMS.WEB/Areas/Admin/Models/Dashboard/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ELMS.WEB.Areas.Equipment.Models;
+using ELMS.WEB.Areas.Loan.Models;
+using System.Collections.Generic;
+
+namespace ELMS.WEB.Areas.Admin.Models.Dashboard
+{
+    public class IndexViewModel
+    {
+        public IList<LoanViewModel> Loans { get; set; } = new List<LoanViewModel>();
+        public IList<EquipmentViewModel> EquipmentList { get; set; } = new List<EquipmentViewModel>();
+    }
+}
