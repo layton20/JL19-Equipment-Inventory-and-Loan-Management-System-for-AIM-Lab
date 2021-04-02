79554019e43862d0c350088552adf68648281528
Johnny Le <contact.lejohnny@gmail.com>
2021-04-02 23:51:37 +0100

    Added blob and user unit tests

---
+++ ELMS.WEB/ELMS.XUNIT/General/BlobUnitTest.cs
@@ -0,0 +1,205 @@
+﻿using Bogus;
+using ELMS.WEB.Entities.General;
+using ELMS.WEB.Repositories.General.Interface;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ELMS.XUNIT.General
+{
+    public class BlobUnitTest
+    {
+        private readonly Mock<IBlobRepository> __BlobRepository = new Mock<IBlobRepository>();
+
+        [Fact]
+        public async Task CreateAsync_ShouldReturnBlob_WhenBlobIsValid()
+        {
+            // Arrange
+            Faker<BlobEntity> _BlobFaker = new Faker<BlobEntity>()
+                .RuleFor(o => o.Name, f => f.System.FileName())
+                .RuleFor(o => o.Path, f => f.Image.PicsumUrl());
+
+            BlobEntity _BlobFake = _BlobFaker.Generate();
+
+            __BlobRepository.Setup(x => x.CreateAsync(_BlobFake)).ReturnsAsync(_BlobFake);
+
+            // Act
+            BlobEntity _Blob = await __BlobRepository.Object.CreateAsync(_BlobFake);
+
+            // Assert
+            Assert.Equal(_BlobFake, _Blob);
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldNotReturnBlob_WhenBlobIsNotValid()
+        {
+            // Arrange
+            __BlobRepository.Setup(x => x.CreateAsync(It.Is<BlobEntity>(x => x.UID == Guid.Empty))).ReturnsAsync(() => null);
+
+            // Act
+            BlobEntity _Blob = await __BlobRepository.Object.CreateAsync(new BlobEntity { UID = Guid.Empty });
+
+            // Assert
+            Assert.Null(_Blob);
+        }
+
+        [Fact]
+        public async Task GetAsync_ShouldReturnBlobs_WhenBlobsExists()
+        {
+            // Arrange
+            Faker<BlobEntity> _BlobFaker = new Faker<BlobEntity>()
+                .RuleFor(o => o.Name, f => f.System.FileName())
+                .RuleFor(o => o.Path, f => f.Image.PicsumUrl());
+
+            IList<BlobEntity> _BlobFakes = _BlobFaker.Generate(5);
+
+            __BlobRepository.Setup(x => x.GetAsync()).ReturnsAsync(_BlobFakes);
+
+            // Act
+            IList<BlobEntity> _Blobs = await __BlobRepository.Object.GetAsync();
+
+            // Assert
+            Assert.Equal(5, _Blobs.Count());
+        }
+
+        [Fact]
+        public async Task GetAsync_ShouldNotReturnBlobs_WhenBlobsDoNotExist()
+        {
+            // Arrange
+            __BlobRepository.Setup(x => x.GetAsync()).ReturnsAsync(() => null);
+
+            // Act
+            IList<BlobEntity> _Blobs = await __BlobRepository.Object.GetAsync();
+
+            // Assert
+            Assert.Null(_Blobs);
+        }
+
+        [Fact]
+        public async Task GetByUIDAsync_ShouldReturnBlob_WhenBlobExists()
+        {
+            // Arrange
+            Faker<BlobEntity> _BlobFaker = new Faker<BlobEntity>()
+                .RuleFor(o => o.Name, f => f.System.FileName())
+                .RuleFor(o => o.Path, f => f.Image.PicsumUrl());
+
+            IList<BlobEntity> _BlobFakes = _BlobFaker.Generate(5);
+
+            __BlobRepository.Setup(x => x.GetAsync(It.IsAny<Guid>())).ReturnsAsync((Guid uid) => _BlobFakes.FirstOrDefault(x => x.UID == uid));
+
+            // Act
+            BlobEntity _Blob = await __BlobRepository.Object.GetAsync(_BlobFakes[0].UID);
+
+            // Assert
+            Assert.Equal(_BlobFakes[0], _Blob);
+        }
+
+        [Fact]
+        public async Task GetByUIDAsync_ShouldNotReturnBlob_WhenBlobDoesNotExist()
+        {
+            // Arrange
+            Faker<BlobEntity> _BlobFaker = new Faker<BlobEntity>()
+                .RuleFor(o => o.Name, f => f.System.FileName())
+                .RuleFor(o => o.Path, f => f.Image.PicsumUrl());
+
+            IList<BlobEntity> _BlobFakes = _BlobFaker.Generate(5);
+
+            __BlobRepository.Setup(x => x.GetAsync(Guid.Empty)).ReturnsAsync(() => null);
+            __BlobRepository.Setup(x => x.GetAsync(It.Is<Guid>(x => !_BlobFakes.Select(b => b.UID).Contains(x)))).ReturnsAsync(() => null);
+
+            // Act
+            BlobEntity _ResultEmptyGuid = await __BlobRepository.Object.GetAsync(Guid.Empty);
+            BlobEntity _ResultNonExisting = await __BlobRepository.Object.GetAsync(Guid.NewGuid());
+
+            // Assert
+            Assert.Null(_ResultEmptyGuid);
+            Assert.Null(_ResultNonExisting);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_ShouldReturnBlob_WhenBlobExists()
+        {
+            // Arrange
+            Faker<BlobEntity> _BlobFaker = new Faker<BlobEntity>()
+                .RuleFor(o => o.Name, f => f.System.FileName())
+                .RuleFor(o => o.Path, f => f.Image.PicsumUrl());
+
+            IList<BlobEntity> _BlobFakes = _BlobFaker.Generate(5);
+
+            __BlobRepository.Setup(x => x.DeleteAsync(It.Is<Guid>(x => _BlobFakes.Select(b => b.UID).Contains(x)))).ReturnsAsync(true);
+
+            // Act
+            bool _Result = await __BlobRepository.Object.DeleteAsync(_BlobFakes[0].UID);
+
+            // Assert
+            Assert.True(_Result);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_ShouldNotReturnBlob_WhenBlobDoesNotExist()
+        {
+            // Arrange
+            Faker<BlobEntity> _BlobFaker = new Faker<BlobEntity>()
+                .RuleFor(o => o.Name, f => f.System.FileName())
+                .RuleFor(o => o.Path, f => f.Image.PicsumUrl());
+
+            IList<BlobEntity> _BlobFakes = _BlobFaker.Generate(5);
+
+            __BlobRepository.Setup(x => x.DeleteAsync(Guid.Empty)).ReturnsAsync(false);
+            __BlobRepository.Setup(x => x.DeleteAsync(It.Is<Guid>(x => !_BlobFakes.Select(b => b.UID).Contains(x)))).ReturnsAsync(false);
+
+            // Act
+            bool _ResultEmptyGuid = await __BlobRepository.Object.DeleteAsync(Guid.Empty);
+            bool _ResultNonExisting = await __BlobRepository.Object.DeleteAsync(Guid.NewGuid());
+
+            // Assert
+            Assert.False(_ResultEmptyGuid);
+            Assert.False(_ResultNonExisting);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_ShouldReturnTrue_WhenBlobIsValid()
+        {
+            // Arrange
+            Faker<BlobEntity> _BlobFaker = new Faker<BlobEntity>()
+                .RuleFor(o => o.Name, f => f.System.FileName())
+                .RuleFor(o => o.Path, f => f.Image.PicsumUrl());
+
+            IList<BlobEntity> _BlobFakes = _BlobFaker.Generate(5);
+
+            __BlobRepository.Setup(x => x.UpdateAsync(It.Is<BlobEntity>(x => _BlobFakes.Contains(x)))).ReturnsAsync(true);
+
+            // Act
+            bool _Result = await __BlobRepository.Object.UpdateAsync(_BlobFakes[0]);
+
+            // Assert
+            Assert.True(_Result);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_ShouldReturnFalse_WhenBlobIsNotValid()
+        {
+            // Arrange
+            Faker<BlobEntity> _BlobFaker = new Faker<BlobEntity>()
+                .RuleFor(o => o.Name, f => f.System.FileName())
+                .RuleFor(o => o.Path, f => f.Image.PicsumUrl());
+
+            IList<BlobEntity> _BlobFakes = _BlobFaker.Generate(5);
+
+            __BlobRepository.Setup(x => x.UpdateAsync(It.Is<BlobEntity>(x => x.UID == Guid.Empty))).ReturnsAsync(false);
+            __BlobRepository.Setup(x => x.UpdateAsync(It.Is<BlobEntity>(x => !_BlobFakes.Contains(x)))).ReturnsAsync(false);
+
+            // Act
+            bool _ResultEmptyGuid = await __BlobRepository.Object.UpdateAsync(new BlobEntity { UID = Guid.Empty });
+            bool _ResultNonExisting = await __BlobRepository.Object.UpdateAsync(_BlobFaker.Generate());
+
+            // Assert
+            Assert.False(_ResultEmptyGuid);
+            Assert.False(_ResultNonExisting);
+        }
+    }
+}
---
+++ ELMS.WEB/ELMS.XUNIT/Identity/UserUnitTests.cs
@@ -0,0 +1,182 @@
+﻿using Bogus;
+using ELMS.WEB.Repositories.Identity.Interface;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ELMS.XUNIT.Identity
+{
+    public class UserUnitTests
+    {
+        private readonly Mock<IUserRepository> __NoteRepositoryMock = new Mock<IUserRepository>();
+
+        [Fact]
+        public async Task GetAsync_ShouldReturnUsers_WhenUsersExists()
+        {
+            // Arrange
+            Faker<IdentityUser> _IdentityFaker = new Faker<IdentityUser>()
+                .RuleFor(o => o.Email, f => f.Person.Email)
+                .RuleFor(o => o.UserName, f => f.Person.UserName)
+                .RuleFor(o => o.EmailConfirmed, f => f.Random.Bool())
+                .RuleFor(o => o.Id, f => f.Random.Guid().ToString());
+
+            IList<IdentityUser> _UserFakes = _IdentityFaker.Generate(5);
+
+            __NoteRepositoryMock.Setup(x => x.GetAsync()).ReturnsAsync(_UserFakes);
+
+            // Act
+            IList<IdentityUser> _Users = await __NoteRepositoryMock.Object.GetAsync();
+
+            // Assert
+            Assert.Equal(_UserFakes, _Users);
+        }
+
+        [Fact]
+        public async Task GetAsync_ShouldNotReturnUsers_WhenUsersDoNotExist()
+        {
+            // Arrange
+            __NoteRepositoryMock.Setup(x => x.GetAsync()).ReturnsAsync(() => null);
+
+            // Act
+            IList<IdentityUser> _Users = await __NoteRepositoryMock.Object.GetAsync();
+
+            // Assert
+            Assert.Null(_Users);
+        }
+
+        [Fact]
+        public async Task GetByEmailAsync_ShouldReturnUsers_WhenUsersExists()
+        {
+            // Arrange
+            Faker<IdentityUser> _IdentityFaker = new Faker<IdentityUser>()
+                .RuleFor(o => o.Email, f => f.Person.Email)
+                .RuleFor(o => o.UserName, f => f.Person.UserName)
+                .RuleFor(o => o.EmailConfirmed, f => f.Random.Bool())
+                .RuleFor(o => o.Id, f => f.Random.Guid().ToString());
+
+            IList<IdentityUser> _UserFakes = _IdentityFaker.Generate(5);
+
+            __NoteRepositoryMock.Setup(x => x.GetAsync(It.IsAny<string>())).ReturnsAsync((string email) => _UserFakes.FirstOrDefault(x => x.Email.ToUpper() == email.ToUpper()));
+
+            // Act
+            IdentityUser _User = await __NoteRepositoryMock.Object.GetAsync(_UserFakes[0].Email);
+
+            // Assert
+            Assert.Equal(_UserFakes[0], _User);
+        }
+
+        [Fact]
+        public async Task GetByEmailAsync_ShouldNotReturnUsers_WhenUsersDoNotExist()
+        {
+            // Arrange
+            Faker<IdentityUser> _IdentityFaker = new Faker<IdentityUser>()
+                .RuleFor(o => o.Email, f => f.Person.Email)
+                .RuleFor(o => o.UserName, f => f.Person.UserName)
+                .RuleFor(o => o.EmailConfirmed, f => f.Random.Bool())
+                .RuleFor(o => o.Id, f => f.Random.Guid().ToString());
+
+            IList<IdentityUser> _UserFakes = _IdentityFaker.Generate(5);
+
+            __NoteRepositoryMock.Setup(x => x.GetAsync(It.IsAny<string>())).ReturnsAsync((string email) => _UserFakes.FirstOrDefault(x => x.Email.ToUpper() == email.ToUpper()));
+
+            // Act
+            IdentityUser _User = await __NoteRepositoryMock.Object.GetAsync($"{Guid.NewGuid()}");
+
+            // Assert
+            Assert.Null(_User);
+        }
+
+        [Fact]
+        public async Task GetByUIDAsync_ShouldReturnUser_WhenUserExists()
+        {
+            // Arrange
+            Faker<IdentityUser> _IdentityFaker = new Faker<IdentityUser>()
+                .RuleFor(o => o.Email, f => f.Person.Email)
+                .RuleFor(o => o.UserName, f => f.Person.UserName)
+                .RuleFor(o => o.EmailConfirmed, f => f.Random.Bool())
+                .RuleFor(o => o.Id, f => f.Random.Guid().ToString());
+
+            IList<IdentityUser> _UserFakes = _IdentityFaker.Generate(5);
+
+            __NoteRepositoryMock.Setup(x => x.GetByUIDAsync(It.IsAny<Guid>())).ReturnsAsync((Guid uid) => _UserFakes.FirstOrDefault(x => x.Id == uid.ToString()));
+
+            // Act
+            Guid.TryParse(_UserFakes[0].Id, out Guid _Uid);
+            IdentityUser _User = await __NoteRepositoryMock.Object.GetByUIDAsync(_Uid);
+
+            // Assert
+            Assert.Equal(_UserFakes[0], _User);
+        }
+
+        [Fact]
+        public async Task GetByUIDAsync_ShouldNotReturnUser_WhenUserDoesNotExist()
+        {
+            // Arrange
+            Faker<IdentityUser> _IdentityFaker = new Faker<IdentityUser>()
+                .RuleFor(o => o.Email, f => f.Person.Email)
+                .RuleFor(o => o.UserName, f => f.Person.UserName)
+                .RuleFor(o => o.EmailConfirmed, f => f.Random.Bool())
+                .RuleFor(o => o.Id, f => f.Random.Guid().ToString());
+
+            IList<IdentityUser> _UserFakes = _IdentityFaker.Generate(5);
+
+            __NoteRepositoryMock.Setup(x => x.GetByUIDAsync(It.IsAny<Guid>())).ReturnsAsync((Guid uid) => _UserFakes.FirstOrDefault(x => x.Id == uid.ToString()));
+
+            // Act
+            IdentityUser _User = await __NoteRepositoryMock.Object.GetByUIDAsync(Guid.NewGuid());
+
+            // Assert
+            Assert.Null(_User);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_ShouldReturnTrue_WhenUserExists()
+        {
+            // Arrange
+            Faker<IdentityUser> _IdentityFaker = new Faker<IdentityUser>()
+                .RuleFor(o => o.Email, f => f.Person.Email)
+                .RuleFor(o => o.UserName, f => f.Person.UserName)
+                .RuleFor(o => o.EmailConfirmed, f => f.Random.Bool())
+                .RuleFor(o => o.Id, f => f.Random.Guid().ToString());
+
+            IList<IdentityUser> _UserFakes = _IdentityFaker.Generate(5);
+
+            __NoteRepositoryMock.Setup(x => x.DeleteAsync(It.IsAny<Guid>())).ReturnsAsync((Guid uid) => _UserFakes.Select(x => x.Id).Contains(uid.ToString()));
+
+            // Act
+            Guid.TryParse(_UserFakes[0].Id, out Guid uid);
+            bool _Result = await __NoteRepositoryMock.Object.DeleteAsync(uid);
+
+            // Assert
+            Assert.True(_Result);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_ShouldReturnFalse_WhenUserDoesNotExist()
+        {
+            // Arrange
+            Faker<IdentityUser> _IdentityFaker = new Faker<IdentityUser>()
+                .RuleFor(o => o.Email, f => f.Person.Email)
+                .RuleFor(o => o.UserName, f => f.Person.UserName)
+                .RuleFor(o => o.EmailConfirmed, f => f.Random.Bool())
+                .RuleFor(o => o.Id, f => f.Random.Guid().ToString());
+
+            IList<IdentityUser> _UserFakes = _IdentityFaker.Generate(5);
+
+            __NoteRepositoryMock.Setup(x => x.DeleteAsync(Guid.Empty)).ReturnsAsync(false);
+            __NoteRepositoryMock.Setup(x => x.DeleteAsync(It.IsAny<Guid>())).ReturnsAsync((Guid uid) => _UserFakes.Select(x => x.Id).Contains(uid.ToString()));
+
+            // Act
+            bool _ResultEmptyGuid = await __NoteRepositoryMock.Object.DeleteAsync(Guid.Empty);
+            bool _ResultNonExisting = await __NoteRepositoryMock.Object.DeleteAsync(Guid.NewGuid());
+
+            // Assert
+            Assert.False(_ResultNonExisting);
+            Assert.False(_ResultEmptyGuid);
+        }
+    }
+}
